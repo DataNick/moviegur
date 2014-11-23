4798f1743b0734775ede6500cee59de5237f7d4c
DataNick <DataNick@users.noreply.github.com>
2014-11-22 23:09:57 -0500

    Making each item show on own url using dynamic routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,4 +18,25 @@ class MoviesController < ApplicationController
 		      }
 		    ]
 	end
+
+	def show
+		@movies = [
+		      {
+		        :title  => "The old church on the coast of White sea",
+		        :director => "Sergey Ershov",
+		        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+		      },
+		      {
+		        :title  => "Sea Power",
+		        :director => "Stephen Scullion",
+		        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+		      },
+		      {
+		        :title  => "Into the Poppies",
+		        :director => "John Wilhelm",
+		        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+		      }
+		    ]
+		    @movie = @movies[params[:id].to_i]
+	end
 end
