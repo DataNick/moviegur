0ff1d84a1cdbbd05175f7e074b6e3a0babaf9f86
DataNick <DataNick@users.noreply.github.com>
2014-11-22 23:07:19 -0500

    making movies show at /movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,21 @@
 class MoviesController < ApplicationController
 	def index
+		@movies = [
+		      {
+		        :title  => "The old church on the coast of White sea",
+		        :director => "Sergey Ershov",
+		        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+		      },
+		      {
+		        :title  => "Sea Power",
+		        :director => "Stephen Scullion",
+		        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+		      },
+		      {
+		        :title  => "Into the Poppies",
+		        :director => "John Wilhelm",
+		        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+		      }
+		    ]
 	end
 end
